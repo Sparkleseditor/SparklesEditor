b48ece442988d295d2fe1c2423c13b7c960b50cf
SyntaxSpin <86142168+SyntaxSpin@users.noreply.github.com>
2026-02-17 15:21:00 +0100

    chore : editor symbolinputview improvements

--- app/src/main/java/com/sparkleseditor/fragments/MainFragment.java
+++ app/src/main/java/com/sparkleseditor/fragments/MainFragment.java
@@ -1,23 +1,16 @@
 package com.sparkleseditor.fragments;
 
-import static android.R.color.transparent;
-
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sparkleseditor.R;
 import com.sparkleseditor.components.ExpandableLayout;
 import com.sparkleseditor.databinding.FragmentMainBinding;
